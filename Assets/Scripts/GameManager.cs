2f838d3b5aaa580434e16d15881ee18d65fa7aaf
Philippe Lupion <philippelupion@hotmail.com>
2023-10-26 16:12:01 -0300

    Add GameManager script

---
+++ Assets/Scripts/GameManager.cs
@@ -0,0 +1,23 @@
+using UnityEngine;
+
+public class GameManager : MonoBehaviour
+{
+    public static GameManager Instance;
+
+    public PlayerController Player { get; private set; }
+
+    private void Awake()
+    {
+        Instance = this;
+    }
+
+    private void Start()
+    {
+        UiHandlerManager.Instance.Initialize();
+    }
+
+    public void SetPlayerController(PlayerController player)
+    {
+        Player = player;
+    }
+}
