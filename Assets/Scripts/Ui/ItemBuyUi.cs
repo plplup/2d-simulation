7447d4ccf9af8d4734eabb9dfd2d1178085241f5
Philippe Lupion <philippelupion@hotmail.com>
2023-10-26 23:37:05 -0300

    Add ItemBuyUi and ItemSellUi script Add ItemBuyPrefab and ItemSellPrefab

---
+++ Assets/Scripts/Ui/ItemBuyUi.cs
@@ -0,0 +1,11 @@
+using TMPro;
+using UnityEngine;
+using UnityEngine.UI;
+
+public class ItemBuyUi : MonoBehaviour
+{
+    public Button BuyButton;
+    public Image IconImage;
+    public TMP_Text NameText;
+    public TMP_Text PriceText;
+}
---
+++ Assets/Scripts/Ui/ItemSellUi.cs
@@ -0,0 +1,13 @@
+using TMPro;
+using UnityEngine;
+using UnityEngine.UI;
+
+public class ItemSellUi : MonoBehaviour
+{
+    public Button SellButton;
+    public Image IconImage;
+    public TMP_Text NameText;
+    public TMP_Text ValueText;
+    public GameObject IsEquippedPanel;
+
+}
